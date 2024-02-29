9cfb62da761d4633546d1062e276c8fc61e2a788
Furkan <furkan-cizreli@hotmail.com>
2024-02-29 21:03:32 +0300

    ilk commit

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use function Webmozart\Assert\Tests\StaticAnalysis\boolean;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name=fake()->name;
+        return [
+            "category_id"=>random_int(1,10),
+            "user_id"=>random_int(1,10),
+            "name"=>$name,
+            "slug"=>Str::slug($name),
+            "body"=>fake()->paragraph,
+            "status"=>fake()->boolean,
+            "tags"=>Str::slug(fake()->address,", "),
+            "seo_description"=>fake()->text,
+            "seo_keywords"=>Str::slug(fake()->address,", "),
+            "view_count"=>random_int(0,100),
+            "like_count"=>random_int(0,100),
+            "read_time"=>random_int(0,2000),
+            "publish_date"=>fake()->dateTime
+        ];
+    }
+}
